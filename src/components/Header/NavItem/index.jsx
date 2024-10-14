79b1b7e776be745641df96234cef45ad1f69ff63
Richy <wuzi50115@gmail.com>
2024-10-14 20:59:38 +0800

    Update input attributes in username field

--- src/components/Header/NavItem/index.jsx
+++ src/components/Header/NavItem/index.jsx
@@ -92,8 +92,8 @@ const NavItems = () => {
                       id="username"
                       type="text"
                       value={username}
+                      autoComplete="username"
                       required
-                      autoComplete="email"
                       className="block w-full rounded-md border-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset  sm:text-sm sm:leading-6"
                       onChange={(e) => setUsername(e.target.value)}
                     />
