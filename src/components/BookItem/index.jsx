e490efcce54f7c2982635fefe276b610f9f8d0a2
Richy <wuzi50115@gmail.com>
2024-10-19 21:03:44 +0800

    feat: Add BookItem component to display list of books

---
+++ src/components/BookItem/index.jsx
@@ -0,0 +1,42 @@
+const BookItem = ({ books }) => {
+  return (
+    <div className="mt-8">
+      <div>
+        <ul role="list" className="-my-6 divide-y divide-gray-200">
+          {books.map((book) => (
+          <li key={book.id} className="flex py-6">
+          <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
+            <img
+              alt={book.title}
+              src={book.img}
+              className="h-full w-full object-center"
+              />
+          </div>
+          <div className="ml-4 flex flex-1 flex-col">
+            <div>
+              <div className="flex justify-between text-base text-gray-900">
+                <h3>
+                  <p>{book.title}</p>
+                </h3>
+                  <p className="ml-4 text-right text-sm whitespace-nowrap">{book.price}</p>
+              </div>
+              <p className="mt-1 text-sm text-gray-500">{book.author}</p>
+            </div>
+            <div className="flex flex-1 items-end justify-between text-sm">
+              <p className="text-gray-500">{book.series}</p>
+              <div className="flex">
+                <button type="button" className="font-medium text-blue-600 hover:text-blue-500">
+                  移除
+                </button>
+              </div>
+            </div>
+          </div>
+          </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default BookItem
