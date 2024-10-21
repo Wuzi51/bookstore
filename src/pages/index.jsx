756235b28f8b9a48b8f3e6ff557a152978eb512c
Richy <wuzi50115@gmail.com>
2024-10-21 20:12:49 +0800

    feat: Add Favorite component to export and update imports

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,5 +3,6 @@ import NotFound from "./NotFound";
 import Book from "./Book";
 import Books from "./Books";
 import Checkout from "./Checkout";
+import Favorite from "./Favorite";
 
-export { Home, NotFound, Book, Books, Checkout  }
+export { Home, NotFound, Book, Books, Checkout, Favorite }
