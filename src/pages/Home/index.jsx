a545d920aaa1b2517d3a47c7f6a9a361a8b9a6c7
Richy <wuzi50115@gmail.com>
2024-10-21 20:14:04 +0800

    refactor: Use global state for books and manage favorites

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,18 +3,36 @@ import BookCard from "@/components/BookCard"
 import { useNavigate } from "react-router-dom"
 import { bookApi } from "@/api/book"
 import "./index.css"
-import { useEffect, useState } from "react"
+import { useEffect } from "react"
+import { useBookStore } from "@/store/book"
+
 
 const Home = () => {
+  const { books, setBooks, favoriteBooks, setFavoriteBooks } = useBookStore()
+
   const getBooks = async() => {
     const { data } = await bookApi.getBooks()
-    setBooks(data)
-    console.log(data)
+    setBooks(data)//存進store
   }
-  const [books, setBooks] = useState([])
+
+  const handleFavoriteClick = (id) => {
+    const isFavorite = books.some((item) => item.id === id)
+    const book = books.find((item) => item.id === id)
+    if (isFavorite) {
+      //移除收藏
+      setFavoriteBooks(id)
+    } else {
+      //加入收藏
+      setFavoriteBooks([...favoriteBooks, book])
+    }
+  }
+
+  const handleCartClick = (id) => {
+    console.log(id)
+  }
 
   const navigate = useNavigate()
-  
+
   const changePage = (url) => {
     navigate(url)
   }
@@ -32,7 +50,8 @@ const Home = () => {
         <Category title="Today's_Picks" />
         <div className="book-list">
           {books.map(book => (
-            <BookCard book={book} key={book.id} onClick={() => changePage(`/book/${book.id}`)}/>
+            <BookCard book={book} key={book.id} onClick={() => changePage(`/book/${book.id}`)}
+              onFavoriteClick={handleFavoriteClick}  onCartClick={handleCartClick}/>
           ))}
         </div>
       </div>
