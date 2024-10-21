e72616dca72f67869cb8e1f7ae3b7b64b2d1c2af
Richy <wuzi50115@gmail.com>
2024-10-21 20:15:11 +0800

    feat: Add dark mode state management to store

--- src/store/user.js
+++ src/store/user.js
@@ -8,6 +8,8 @@ export const useUserStore = create(
       setToken: (token) => set({ token }),
       language: "zh_TW",
       setLanguage: (language) => set({ language }),
+      darkMode: false,
+      setDarkMode: (mode) => set({ darkMode: mode })
     }),
     {
       name: 'user',
