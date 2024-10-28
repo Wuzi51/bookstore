60f1efb5a51a8352cf5d13c30b629b984938a6be
Richy <wuzi50115@gmail.com>
2024-10-28 20:49:04 +0800

    Refactor Favorite component to use Zustand global state for favorite books

--- src/pages/Favorite/index.jsx
+++ src/pages/Favorite/index.jsx
@@ -1,21 +1,8 @@
 import BookCard from "@/components/BookCard"
-// import { useState, useEffect } from "react"
-// import { bookApi } from "@/api/book"
 import { useBookStore } from "@/store/book"
 
 const Favorite = () => {
   const { favoriteBooks } = useBookStore()
-  // const [books, setBooks] = useState([])
-  
-  // const getBooks = async() => {
-  //   const {data} = await bookApi.getBooks()
-  //   setBooks(data)
-  // }
-
-  // useEffect(() => {
-  // getBooks()
-  // }, [])
-  
   return (
     <div className="mt-3">
       <h2 className="text-2xl ml-3 font-bold mb-2">我的願望清單</h2>
