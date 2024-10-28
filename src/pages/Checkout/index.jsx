98c87447b05e97f357c2fc6635a8dcb1fa9df588
Richy <wuzi50115@gmail.com>
2024-10-28 19:46:05 +0800

    feat: Complete checkout feature with form submission

--- src/pages/Checkout/index.jsx
+++ src/pages/Checkout/index.jsx
@@ -1,19 +1,43 @@
 import BookItem from "@/components/BookItem";
 import { message } from "antd";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useBookStore } from "@/store/book";
+
 const Checkout = () => {
-  //使用全局狀態獲取加入購物車的資料
-  const { cart, totalPrice, removeCart } = useBookStore()
+  //使用全局狀態獲取資料
+  const { cart, getTotalPrice, removeCart, orderList, setOrderList } = useBookStore()
+  const [payment, setPayment] = useState('visa')
+  const [cardNumber, setCardNumber] = useState('')
+  const [expirationMonth, setExpirationMonth] = useState('')
+  const [expirationYear, setExpirationYear] = useState('')
+  const [cvv, setCvv] = useState('')
 
   const handleRemoveClick = (idx) => {
     removeCart(idx)
-  }
+  }  
 
   const navigate = useNavigate()
   const changePage = (url) => {
     navigate(url)
     message.success('付款成功')
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const orderDate = {
+      payment,
+      cardNumber,
+      expirationMonth,
+      expirationYear,
+      cvv,
+      createdAt: Math.round(new Date().getTime / 1000),
+      products: cart,
+      totalPrice: getTotalPrice(),
+    }
+    setOrderList([...orderList, orderDate])
+    console.log(orderList)
+    changePage('/')
   }
 
   const monthDropdown = () => {
@@ -47,9 +71,15 @@ const Checkout = () => {
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
         <div className="bg-white shadow-lg rounded-lg p-6">
           <h3 className="text-2xl font-semibold mb-6 text-gray-800">付款</h3>
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="mb-4">
-              <input type="radio" id="credit-card" name="payment" className="mr-2" />
+              <input type="radio" 
+              id="credit-card" 
+              name="payment" 
+              className="mr-2"
+              checked={payment === 'visa'}
+              onChange={() => setPayment('visa')}
+              />
               <label htmlFor="credit-card" className="text-lg font-medium text-gray-700">
                 信用卡/Visa 金融卡
               </label>
@@ -60,6 +90,8 @@ const Checkout = () => {
               </label>
               <input
                 type="text"
+                value={cardNumber}
+                onChange={(e) => setCardNumber(e.target.value)}
                 id="card-number"
                 className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
                 placeholder="1234 5678 9012 3456"
@@ -71,6 +103,8 @@ const Checkout = () => {
               </label>
               <div className="flex space-x-2">
                 <select
+                  value={expirationMonth}
+                  onChange={(e) => setExpirationMonth(e.target.value)}
                   className="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
                 >
                   <option value="" disabled selected>
@@ -80,6 +114,8 @@ const Checkout = () => {
                 </select>
                 <p className="flex items-center">/</p>
                 <select
+                  value={expirationYear}
+                  onChange={(e) => setExpirationYear(e.target.value)}
                   className="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
                 >
                   <option value="" disabled selected>
@@ -94,19 +130,26 @@ const Checkout = () => {
               <input
                 type="text"
                 id="cvv"
+                value={cvv}
+                onChange={(e) => setCvv(e.target.value)}
                 className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
                 placeholder="123"
               />
             </div>
             <div className="mb-6">
-              <input type="radio" id="apple-pay" name="payment" className="mr-2" />
+              <input type="radio" 
+              id="apple-pay" 
+              name="payment" 
+              className="mr-2" 
+              checked={payment === 'applepay'}
+              onChange={() => setPayment('applepay')}
+              />
               <label htmlFor="apple-pay" className="text-lg font-medium text-gray-700">
                 Apple Pay
               </label>
             </div>
             <button
-              type="button"
-              className="w-full bg-blue-500 text-white py-3 px-4 rounded-md hover:bg-blue-400 transition duration-300" onClick={() => changePage('/')}
+              className="w-full bg-blue-500 text-white py-3 px-4 rounded-md hover:bg-blue-400 transition duration-300" onClick={handleSubmit}
             >
               確定付款
             </button>
@@ -119,7 +162,7 @@ const Checkout = () => {
           <p className="my-4 border border-solid border-stone-500"></p>
           <div className="flex justify-between text-base font-medium text-gray-900">
             <p>總金額</p>
-            <p>NT${totalPrice()}</p>
+            <p>NT${getTotalPrice()}</p>
           </div>
         </div>
       </div>
