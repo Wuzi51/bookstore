7fdf650d72913dc599eb74280a9c8a029c717a0d
Richy <wuzi50115@gmail.com>
2024-10-31 17:12:36 +0800

    feat: Enhance Favorite component with navigation and event handlers

--- src/pages/Favorite/index.jsx
+++ src/pages/Favorite/index.jsx
@@ -1,14 +1,32 @@
 import BookCard from "@/components/BookCard"
 import { useBookStore } from "@/store/book"
+import { useNavigate } from "react-router-dom"
 
 const Favorite = () => {
-  const { favoriteBooks } = useBookStore()
+  const { favoriteBooks, setFavoriteBooks, setCart } = useBookStore()
+  const navigate = useNavigate()
+
+  const changePage = (url) => {
+    navigate(url)
+  }
+
+  const handleFavoriteClick = (id) => {
+    setFavoriteBooks(id)
+  }
+
+  const handleCartClick = (id, qty = 1) => {
+    setCart(id, qty)
+  }
+
   return (
     <div className="mt-3">
       <h2 className="text-2xl ml-3 font-bold mb-2">我的願望清單</h2>
       <div className="flex">
         {favoriteBooks.map(book => (
-          <BookCard book={book} key={book.id} showButtons={false}/>
+          <BookCard book={book} key={book.id}
+          onFavoriteClick={handleFavoriteClick}
+          onCartClick={handleCartClick}
+          onClick={() => changePage(`/book/${book.id}`)}/>
         ))}
       </div>
     </div>
