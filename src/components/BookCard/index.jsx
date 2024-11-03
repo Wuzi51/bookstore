085bcb149d7aee377e453652e5e6f9535eb48591
Richy <wuzi50115@gmail.com>
2024-11-03 13:05:42 +0800

    refactor: Remove `showButtons` prop from BookCard component

--- src/components/BookCard/index.jsx
+++ src/components/BookCard/index.jsx
@@ -2,8 +2,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCartShopping, faHeart } from '@fortawesome/free-solid-svg-icons'
 import "./index.css"
 
-//將showButtons預設為true
-const BookCard = ({ book, onClick, onFavoriteClick, onCartClick, showButtons = true}) => {
+const BookCard = ({ book, onClick, onFavoriteClick, onCartClick }) => {
   return (
     <div key={book.id} className="book">
       <div className='book-card' onClick={onClick}>
@@ -13,20 +12,18 @@ const BookCard = ({ book, onClick, onFavoriteClick, onCartClick, showButtons = t
         <p>{`NT$ ${book.price}`}</p>
       </div>
       
-      {showButtons && ( 
-        <div className='btn-area flex justify-evenly mt-3'>
-          <div className='heart-btn' onClick={() => onFavoriteClick(book.id)}>
-            <button>
-              <FontAwesomeIcon icon={faHeart}/>
-            </button>
-          </div>
-          <div className='cart-btn' onClick={() => onCartClick(book.id)}>
-            <button>
-              <FontAwesomeIcon icon={faCartShopping}/>
-            </button>
-          </div>
+      <div className='btn-area flex justify-evenly mt-3'>
+        <div className='heart-btn' onClick={() => onFavoriteClick(book.id)}>
+          <button>
+            <FontAwesomeIcon icon={faHeart}/>
+          </button>
         </div>
-      )}
+        <div className='cart-btn' onClick={() => onCartClick(book.id)}>
+          <button>
+            <FontAwesomeIcon icon={faCartShopping}/>
+          </button>
+        </div>
+      </div>
     </div> 
   )
 }
