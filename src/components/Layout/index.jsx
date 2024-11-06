989e511fa34bd9b925a32e2b4258cd44b70441b9
Richy <wuzi50115@gmail.com>
2024-11-07 06:29:14 +0800

    feat: add smooth scroll to top on route change in Layout component

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,12 +1,22 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Header from "../Header";
 import Footer from "../Footer";
 import { clsx } from "clsx";
 import { useUserStore } from "@/store/user";
+import { useEffect } from "react";
 
 
 const Layout = () => {
-  const { darkMode } = useUserStore()
+  const location = useLocation();
+  const { darkMode } = useUserStore();
+  
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    })
+  }, [location]);
+
   return (
     <div className={clsx({ darkMode }, "flex flex-col min-h-screen")}>
       <Header/>
