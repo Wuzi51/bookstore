13668dc54375b06d74aa78f049eaa76b542491d4
Richy <wuzi50115@gmail.com>
2024-11-18 20:04:53 +0800

    feat: add page navigation and handle search functionality on Enter key press

--- src/components/Header/SearchForm/index.jsx
+++ src/components/Header/SearchForm/index.jsx
@@ -1,13 +1,28 @@
 import { useTranslation } from "react-i18next"
 import "./index.css"
+import { useNavigate } from "react-router-dom"
 
 
 const SearchForm = () => {
-  const { t }= useTranslation()
+  const { t }= useTranslation();
+
+  const navigate = useNavigate();
+
+  const changePage = (url) => {
+    navigate(url)
+  };
+
+  const handleSearch = (event) => {
+    if (event.key === 'Enter') {
+      changePage(`/search?keyword=${event.target.value}`)
+    }
+  }
+  
+
   return (
     <form className="d-flex">
     <input className="form-control searchForm" type="search" placeholder={t('Please_enter_a_keyword')} aria-label="Search" />
-    <button className="btn bg-blue-500" type="submit">{t('search')}</button>
+    <button className="btn bg-blue-500" type="submit" onClick={handleSearch}>{t('search')}</button>
   </form>
   )
 }
