014474cf631230281a156d0e76af54493543d348
Richy <wuzi50115@gmail.com>
2024-11-18 20:00:10 +0800

    feat: integrate eBook reader modal in BookCard component

--- src/components/BookCard/index.jsx
+++ src/components/BookCard/index.jsx
@@ -1,31 +1,53 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCartShopping, faHeart } from '@fortawesome/free-solid-svg-icons'
-import "./index.css"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartShopping, faHeart, faBook } from "@fortawesome/free-solid-svg-icons";
+import "./index.css";
+import { Modal } from "antd";
+import { useState } from "react";
+import EBookReader from "../EBookReader";
 
 const BookCard = ({ book, onClick, onFavoriteClick, onCartClick }) => {
-  return (
-    <div key={book.id} className="book">
-      <div className='book-card' onClick={onClick}>
-        <img src={book.img} alt={book.title}/>
-        <h3>{book.title}</h3>
-        <h4>{book.author}</h4>
-        <p>{`NT$ ${book.price}`}</p>
-      </div>
-      
-      <div className='btn-area flex justify-evenly mt-3'>
-        <div className='heart-btn' onClick={() => onFavoriteClick(book.id)}>
-          <button>
-            <FontAwesomeIcon icon={faHeart}/>
-          </button>
-        </div>
-        <div className='cart-btn' onClick={() => onCartClick(book.id)}>
-          <button>
-            <FontAwesomeIcon icon={faCartShopping}/>
-          </button>
-        </div>
-      </div>
-    </div> 
-  )
-}
+  const [open, setOpen] = useState(false);
 
-export default BookCard
+  return (
+    <>
+      <div key={book.id} className="book">
+        <div className='book-card' onClick={onClick}>
+          <img src={book.img} alt={book.title}/>
+          <h3>{book.title}</h3>
+          <h4>{book.author}</h4>
+          <p>{`NT$ ${book.price}`}</p>
+        </div>
+        
+        <div className='btn-area flex justify-evenly mt-3'>
+          <div className='heart-btn' onClick={() => onFavoriteClick(book.id)}>
+            <button>
+              <FontAwesomeIcon icon={faHeart}/>
+            </button>
+          </div>
+          <div className='book-btn' onClick={() => setOpen(true)}>
+              <button>
+                <FontAwesomeIcon icon={faBook}/>
+              </button>
+          </div>
+          <div className='cart-btn' onClick={() => onCartClick(book.id)}>
+            <button>
+              <FontAwesomeIcon icon={faCartShopping}/>
+            </button>
+          </div>
+        </div>
+      </div> 
+      <Modal
+        title={book.title}
+        open={open}
+        centered
+        onOk={() => setOpen(false)}
+        onCancel={() => setOpen(false)}
+        width={600}
+      >
+        <EBookReader/>
+      </Modal>
+    </>
+  );
+};
+
+export default BookCard;
---
+++ src/components/EBookReader/index.jsx
@@ -0,0 +1,118 @@
+import { useRef, useState, useEffect } from "react";
+import ePub from "epubjs";
+import { Spin } from 'antd'; 
+import { LoadingOutlined } from "@ant-design/icons";
+
+const EBookReader = () => {
+  const ePubRef = useRef(null);
+  const url = 'book.epub';
+  const bookURL = `http://localhost:3000/${url}`;
+  const [rendition, setRendition] = useState();
+  const [progress, setProgress] = useState(0);
+  const [loading, setLoading] = useState();
+
+  const init = async() => {
+    setLoading(true)
+    try {
+      const response = await fetch(bookURL);
+      // 將檔案轉換成可以處理的二進制格式
+      const ePubData = await response.arrayBuffer();
+
+      //產生電子書
+      const book = ePub(ePubData);
+
+      //電子書渲染到DOM元素上
+      const renditionInstance = book.renderTo
+      (ePubRef.current, {
+        width : "100%",
+        height : "100%",
+      });
+      setRendition(renditionInstance);
+
+    // 電子書加載完畢
+      await book.ready;
+      await book.locations.generate(1000);
+       //監聽翻頁事件
+      renditionInstance.on("relocated", (location) => {
+        const currentPercentage = book.locations.percentageFromCfi(location.start.cfi);
+        setProgress(currentPercentage * 100);
+      });
+      // 顯示電子書
+      await renditionInstance.display();
+    } catch(err) {
+      console.error(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handlePrevious = () => {
+    if (rendition) {
+      rendition.prev();   
+    };
+  };
+
+  const handleNext = () => {
+    if (rendition) {
+      rendition.next();
+    };
+  };
+
+  const handleProgressChange = (event) => {
+    const newProgress = event.target.value;
+    setProgress(event.target.value);
+    if (rendition) {
+      // 找到當前在哪一頁 就可跳轉到哪一頁
+      const targetLocation = rendition.book.locations.cfiFromPercentage(
+        newProgress / 100
+      );
+      // 跳轉到當前進度頁
+      rendition.display(targetLocation);
+    }
+  };
+
+  useEffect(() => {
+    init()
+  },[]);
+
+  return (
+    <>
+      <div ref={ePubRef} 
+        className="relative"
+        style={{ width: "100%", height: "30rem", 
+        border: "1px solid #ddd"}}>
+          {loading && (
+            <Spin
+              className="absolute top-1/2 left-1/2 -translate-x-1/2 
+              -translate-y-1/2"
+              indicator={<LoadingOutlined spin spinning={loading} />}
+              size="large"
+            />
+          )}
+      </div>
+        <div style={{marginTop: "10px", textAlign: "center"}} className="mt-3">
+          <input 
+            type="range"
+            min="0"
+            max="100"
+            value={progress}
+            onChange={handleProgressChange}
+            style={{width: "80%"}}
+          />
+          <div style={{marginTop: "10px", textAlign: "center"}}>
+            {loading ? "加載中" : `進度${Math.max(Math.floor(progress), 1)}%`}
+          </div>
+          <div className="mt-3">
+            <button className="rounded-md border border-transparent bg-blue-500 px-2 py-2 mx-2 text-sm font-medium text-white shadow-sm hover:bg-blue-400"
+          onClick={handlePrevious}
+          >上一頁</button>
+            <button className="rounded-md border border-transparent bg-blue-500 px-2 py-2 mx-2 text-sm font-medium text-white shadow-sm hover:bg-blue-400"
+            onClick={handleNext}
+            >下一頁</button>
+          </div>
+        </div>
+    </>
+  );
+};
+
+export default EBookReader;
