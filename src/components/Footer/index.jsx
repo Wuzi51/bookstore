b3c60367061d2d30439bac95bf99d77a7cc9be46
Richy <wuzi50115@gmail.com>
2024-12-17 23:36:07 +0800

    refactor: update footer layout and styling

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,11 +1,10 @@
-import "./index.css"
 import { useTranslation } from "react-i18next";
 
 const Footer = () => {
   const { t } = useTranslation()
   return (
-    <footer className="footer">
-      <div className="container">
+    <footer className="mt-5 bg-gray-100 p-5 text-center border-t border-gray-300">
+      <div className="flex justify-around flex-wrap mb-5">
         <div>
           <h4>{t('welcome')}</h4>
         </div>
@@ -19,6 +18,6 @@ const Footer = () => {
       <p>Copyright © 2024</p>
     </footer>
   );
-}
+};
 
 export default Footer;
