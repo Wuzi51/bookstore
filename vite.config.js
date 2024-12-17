c248bcee409a8ae1c511e4f0c320a91ba2e4319c
Richy <wuzi50115@gmail.com>
2024-12-17 20:43:36 +0800

    refactor: uncomment __filename initialization to enable usage of file path utilities

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 import tailwindcss from 'tailwindcss'
 
-// const __filename = fileURLToPath(import.meta.url);
+const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 
