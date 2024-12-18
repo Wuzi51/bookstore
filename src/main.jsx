333fa39a269975382894c609e03c748d34b472d4
Richy <wuzi50115@gmail.com>
2024-12-18 14:45:40 +0800

    Revert "feat: wrap App component with StrictMode in ReactDOM.createRoot"
    This reverts commit d48b55af7b58e605c0a0ecc4452f0993d0b48fe2.

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-import { StrictMode } from "react"
 import ReactDOM from "react-dom/client"
 import App from './App'
 import "./assets/css/index.css"
@@ -7,8 +6,4 @@ import './mock/index.js'
 
 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
