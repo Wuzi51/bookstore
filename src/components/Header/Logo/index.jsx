85c6023de2d040290fac2096037a241bfaf4c5c2
Richy <wuzi50115@gmail.com>
2025-04-19 18:48:21 +0800

    git commit -m "refactor: replace useNavigate with Link for page transitions"

--- src/components/Header/Logo/index.jsx
+++ src/components/Header/Logo/index.jsx
@@ -1,23 +1,15 @@
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import logo from "@/images/bookstore-high-resolution-logo-transparent.png";
 
 const Logo = () => {
-  const navigate = useNavigate();
-  const changePage = (url) => {
-    navigate(url);
-  };
-
   return (
-    <div
-      onClick={() => changePage("/")}
-      className="flex items-center cursor-pointer md:mt-4"
-    >
+    <Link to="/" className="flex items-center cursor-pointer md:mt-4">
       <img
         src={logo}
         alt="logo"
         className="w-auto h-auto max-w-52 min-w-52 md:max-w-72"
       />
-    </div>
+    </Link>
   );
 };
 
