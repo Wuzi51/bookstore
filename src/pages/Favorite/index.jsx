093454cf416695bd50b726e71d15d80ec0336516
Richy <wuzi50115@gmail.com>
2025-04-29 23:22:24 +0800

    remove: useNavigate hook from Favorite component

--- src/pages/Favorite/index.jsx
+++ src/pages/Favorite/index.jsx
@@ -1,36 +1,29 @@
-import BookCard from "@/components/BookCard"
-import { useBookStore } from "@/store/book"
-import { useNavigate } from "react-router-dom"
+import BookCard from "@/components/BookCard";
+import { useBookStore } from "@/store/book";
 
 const Favorite = () => {
-  const { favoriteBooks, setFavoriteBooks, setCart } = useBookStore()
-  const navigate = useNavigate()
-
-  const changePage = (url) => {
-    navigate(url)
-  }
-
+  const { favoriteBooks, setFavoriteBooks, setCart } = useBookStore();
+  
   const handleFavoriteClick = (id) => {
-    setFavoriteBooks(id)
-  }
+    setFavoriteBooks(id);
+  };
 
   const handleCartClick = (id, qty = 1) => {
-    setCart(id, qty)
-  }
+    setCart(id, qty);
+  };
 
   return (
-    <div className="mt-3">
+    <div className="mt-5">
       <h2 className="text-2xl ml-3 font-bold mb-2">我的願望清單</h2>
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-3">
+      <div className="w-fit grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-3">
         {favoriteBooks.map(book => (
           <BookCard book={book} key={book.id}
           onFavoriteClick={handleFavoriteClick}
-          onCartClick={handleCartClick}
-          onClick={() => changePage(`/book/${book.id}`)}/>
+          onCartClick={handleCartClick} />
         ))}
       </div>
     </div>
   )
-}
+};
 
-export default Favorite
+export default Favorite;
