94763508db2dafd8276467027ffea1b44f0b0f8d
Richy <wuzi50115@gmail.com>
2025-04-29 23:19:20 +0800

    refactor: replace useNavigate with Link for page transitions

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,6 @@
 import Category from "@/components/Category"
 import BookCard from "@/components/BookCard"
-import { useNavigate } from "react-router-dom"
+import { Link } from "react-router-dom"
 import { bookApi } from "@/api/book"
 import { useEffect } from "react"
 import { useBookStore } from "@/store/book"
@@ -28,34 +28,33 @@ const Home = () => {
     setCart(id, qty)
   };
 
-  const navigate = useNavigate();
-
-  const changePage = (url) => {
-    navigate(url)
-  };
-
 useEffect(() => {
     getBooks()
   }, []);
 
   return (
     <div className="flex flex-col items-center justify-between">
-      <div className=" mt-9 hover:cursor-pointer " onClick={() => changePage('/book/15')}>
-        <img 
-          className="hidden sm:block w-[149vh] object-cover hover:scale-105 transition-transform duration-300 ease-in-out" 
-          src={BannerDesktop}
-          alt="desktop banner" />
+      <Link to="/book/15" className=" mt-9 hover:cursor-pointer ">
         <img
-          className="w-full object-cover md:hidden sm:block rounded-l "
-          src={BannerMobile}    
-          alt="mobile banner" />
-      </div>
+          className="hidden md:block w-[149vh] object-cover hover:scale-105 transition-transform duration-300 ease-in-out"
+          src={BannerDesktop}
+          alt="desktop banner"
+        />
+        <img
+          className="block md:hidden w-full object-cover rounded-l"
+          src={BannerMobile}
+          alt="mobile banner"
+        />
+      </Link>
       <div>
         <Category title="Today's_Picks" />
         <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-3">
           {books.map(book => (
-            <BookCard book={book} key={book.id} onClick={() => changePage(`/book/${book.id}`)}
-              onFavoriteClick={handleFavoriteClick}  onCartClick={handleCartClick}/>
+            <BookCard 
+              book={book} 
+              key={book.id} 
+              onFavoriteClick={handleFavoriteClick}
+              onCartClick={handleCartClick}/>
           ))}
         </div>
       </div>
