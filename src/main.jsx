ea83d43d90edb77244cb0530b36f5ae5ab6dfe09
Richy <wuzi50115@gmail.com>
2025-06-28 11:56:50 +0800

    refactor: migrate from mock data to Supabase for book API

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,6 @@ import ReactDOM from "react-dom/client";
 import App from './App';
 import "./assets/css/index.css";
 import "./i18n";
-import './mock/index.js';
-
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
--- src/mock/index.js
+++
@@ -1,7 +0,0 @@
-import Mock from "mockjs";
-import book from "./data/book.json";
-
-Mock.mock('/mock/books', {
-  code: 200,
-  data: book
-});
