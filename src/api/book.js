188b97e93d708598b1d2b9ab781caa73156ee68f
Richy <wuzi50115@gmail.com>
2025-06-28 11:56:50 +0800

    refactor: migrate from mock data to Supabase for book API

--- src/api/book.js
+++ src/api/book.js
@@ -1,8 +1,16 @@
-import axios from "axios";
+import supabase from "@/lib/superbaseClient";
 
 export const bookApi = {
-  getBooks: async() => {
-    const { data } = await axios.get("/mock/books");
-    return data;
-  }
+  getBooks: async () => {
+    const { data, error } = await supabase
+      .from('books')
+      .select('*, comments(*)')
+      // 按照id順序排序
+      .order('id', { ascending: true })
+
+      if (error) {
+        throw error
+      }
+      return { data }
+  },
 };
