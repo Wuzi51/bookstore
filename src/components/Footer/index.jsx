b65c3ace26d5cc4bdeedf87420922ba9e4981d0c
Richy <wuzi50115@gmail.com>
2025-07-30 00:06:26 +0800

    feat: enhance footer with social media links, update wishlist translations, and add return-to-home button on favorite page

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,21 +1,47 @@
 import { useTranslation } from 'react-i18next';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faInstagram, faXTwitter } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
   const { t } = useTranslation();
   return (
-    <footer className="w-screen mt-5 bg-gray-100 p-5 text-center border-t border-gray-300 dark:bg-surface">
-      <div className="grid grid-cols-1 lg:grid-cols-3  gap-4 text-center mb-5">
+    <footer className="mt-5 bg-gray-100 p-3 text-center border-t border-gray-300 dark:bg-surface">
+      <div className="mb-2 grid grid-cols-1 lg:grid-cols-3 gap-4 text-center">
         <div>
           <h4>{t('welcome')}</h4>
         </div>
         <div>
-          <h4 className="text-center">{t('terms_of_service')}</h4>
+          <h4 className="text-center">{t('footer_personal_use')}</h4>
         </div>
-        <div>
-          <h4>{t('social_media')}</h4>
+        <div className='mb-2'>
+          <h4 className='mb-2'>{t('social_media')}</h4>
+          <a
+            href="https://www.facebook.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className='hover:text-gray-600'
+          >
+            <FontAwesomeIcon icon={faFacebook} className="text-xl transition-transform hover:scale-125" />
+          </a>
+          <a
+            href="https://www.instagram.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="ml-4 hover:text-gray-600"
+          >
+            <FontAwesomeIcon icon={faInstagram} className="text-xl transition-transform hover:scale-125" />
+          </a>
+          <a
+            href="https://twitter.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="ml-4"
+          >
+            <FontAwesomeIcon icon={faXTwitter} className="text-xl transition-transform hover:scale-125" />
+          </a>
         </div>
       </div>
-      <p>Copyright © 2024</p>
+      <p className='mt-2 text-s text-gray-400'>Copyright © 2024</p>
     </footer>
   );
 };
--- src/pages/Favorite/index.jsx
+++ src/pages/Favorite/index.jsx
@@ -2,40 +2,57 @@ import BookCard from '@/components/BookCard';
 import { useBookStore } from '@/store/book';
 import { message } from 'antd';
 import { useTranslation } from 'react-i18next';
+import { useUserStore } from '@/store/user';
+import { Link } from 'react-router-dom';
 
 const Favorite = () => {
   const { favoriteBooks, setFavoriteBooks, setCart, cart } = useBookStore();
   const [messageApi, contextHolder] = message.useMessage();
+  const { session } = useUserStore();
   const { t } = useTranslation();
 
   const handleFavoriteClick = (id) => {
     setFavoriteBooks(id);
   };
 
-  const handleCartClick = (id) => {
-    if (cart.some((item) => item.id === id)) {
+  const handleCartClick = async (bookId) => {
+    if (!session?.user) {
+      messageApi.warning(t('Please_Login_First'));
+      return;
+    }
+    if (cart.some(item => item.book_id === bookId)) {
       messageApi.info(t('Already_In_Cart'));
       return;
     }
-    setCart(id);
+    await setCart(session.user.id, bookId);
     messageApi.success(t('Already_Added_To_Cart'));
-  };
+  };;
 
   return (
-    <div className="pt-5">
-      <h2 className="text-2xl ml-3 font-bold mb-2">我的願望清單</h2>
-      <div className="w-fit grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-3">
-        {contextHolder}
-        {favoriteBooks.map((book) => (
-          <BookCard
-            book={book}
-            key={book.id}
-            onFavoriteClick={handleFavoriteClick}
-            onCartClick={handleCartClick}
-          />
-        ))}
+    <>
+      {contextHolder}
+      <div className="pt-5">
+        <h2 className="text-2xl ml-3 font-bold mb-2">{t('My_Wishlist')}</h2>
+        {favoriteBooks.length === 0 ? (
+          <div className='flex flex-col items-center justify-center h-96'>
+            <p className="text-xl text-gray-500 mb-6">{t('Empty_Favorite')}</p>
+            <Link to="/" className='bg-blue-500 text-white py-3 px-4 rounded-md hover:bg-blue-400 transition duration-300'>
+              {t('Go_Shopping')}
+            </Link>
+          </div>
+        ) : null}
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-3">
+          {favoriteBooks.map((book) => (
+            <BookCard
+              book={book}
+              key={book.id}
+              onFavoriteClick={handleFavoriteClick}
+              onCartClick={handleCartClick}
+            />
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
